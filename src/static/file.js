fbbf63f2ea7d3241f110ffe42c5d83c02d777910
alifanantomi <alifanantomi2@gmail.com>
2022-06-25 14:06:17 +0700

    initial commit

---
+++ src/static/file.js
@@ -0,0 +1,10 @@
+export default {
+    arrayProduk: [
+        { nama: 'Buku', harga: 5000, kualitas: 100 },
+        { nama: 'Bolpoin', harga: 6000, kualitas: 70},
+        { nama: 'Pensil', harga: 3000, kualitas: 70},
+        { nama: 'Penggaris', harga: 7000, kualitas: 70},
+        { nama: 'Penghapus', harga: 4000, kualitas: 70},
+        { nama: 'Papan Tulis', harga: 60000, kualitas: 70},
+    ],
+}
---
+++ src/view/HomePage.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+
+        <h1>{{ tampil }}</h1>
+
+        <ul>
+            <li v-for="(item, index) in items" :key="index">
+                {{ item.nama }} - Rp{{ item.harga }} - {{ item.kualitas > 70 ? 'Kualitas Tinggi' : 'Kualitas Rendah' }}
+
+            </li>
+        </ul>
+
+        <h1 v-if="nilai >= 90">
+            Nilai A
+        </h1>
+
+        <h1 v-else-if="nilai < 90 && nilai >=75">
+            Nilai B
+        </h1>
+
+        <h1 v-else>
+            Nilai C
+        </h1>
+    </div>
+</template>
+
+<script>
+import file from "@/static/file.js"
+
+export default {
+    data() {
+        return {
+            tampil: 'Halo World',
+            items: file.arrayProduk,
+            nilai: 100
+        }
+    },
+    mounted() { 
+        console.log("HOME")  
+    }
+}
+</script>
+
+<style>
+
+</style>
